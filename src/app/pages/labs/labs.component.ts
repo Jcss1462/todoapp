d60273c96717cd9782552e3538fc3a7c1ee79785
Juan Camilo Salazar Serna <jcss1462@gmail.com>
2024-06-07 00:09:32 -0500

    configuracionInicialProyecto

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-labs',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [CommonModule],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.scss'
 })
-export class AppComponent {
+export class LabsComponent {
+
   title = 'todoapp';
   welcome ="Holiwis"
   heroes = ["Ironman","Cap","Thor","Hulk","Black widow","Hawk eye"];
+
 }
